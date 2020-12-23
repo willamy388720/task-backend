90acb4bf8efdaf19b66907fc2b679e202ff87edb
willamy388720 <willamygt@gmail.com>
2020-12-23 20:10:07 -0300

    task api

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,17 @@
 module.exports = app => {
   app.post('/signup', app.api.user.save)
   app.post('/signin', app.api.auth.signin)
+
+  app.rout('/tasks')
+    .all(app.config.passport.authenticate())
+    .get(app.api.task.getTask)
+    .post(app.api.task.save)
+
+  app.rout('/tasks/:id')
+    .all(app.config.passport.authenticate())
+    .delete(app.api.task.remove)
+
+  app.rout('/tasks/:id/toggle')
+    .all(app.config.passport.authenticate())
+    .put(app.api.task.toggleTask)
 }
