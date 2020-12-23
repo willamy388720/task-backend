fa1afa4f56e7fe6fd0fa71c04bc83810dbdaa76e
willamy388720 <willamygt@gmail.com>
2020-12-23 17:34:40 -0300

    middlewares and DB with Knex

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+  .then('./config/middleware.js')
+  .into(app)
+
+app.db = db
+
+app.listen(3000, () => {
+  console.log('Backend executando...')
+})
